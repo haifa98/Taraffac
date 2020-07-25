cdc46265f7b2cc418eee965f0c806a7153fe573c
Amjad1418 <amjad.nasser.al@gmail.com>
2020-07-25 07:21:32 +0300

    a

---
+++ app/src/main/java/com/example/taraffac/view_speed_bump.java
@@ -0,0 +1,14 @@
+package com.example.taraffac;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class view_speed_bump extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_speed_bump);
+    }
+}
