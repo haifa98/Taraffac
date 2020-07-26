469f1fb5a08fc87c8aad51d25392fb5f38952f55
Amjad1418 <amjad.nasser.al@gmail.com>
2020-07-26 06:40:43 +0300

    new1

---
+++ app/src/main/java/com/example/taraffac/delete_msg.java
@@ -0,0 +1,14 @@
+package com.example.taraffac;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class delete_msg extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_delete_msg);
+    }
+}
