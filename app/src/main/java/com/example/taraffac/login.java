347398927a1159af76bf86d1d38587f05818ec09
Amjad1418 <amjad.nasser.al@gmail.com>
2020-07-26 08:44:57 +0300

    all class

--- app/src/main/java/com/example/taraffac/login.java
+++ app/src/main/java/com/example/taraffac/login.java
@@ -2,22 +2,30 @@ package com.example.taraffac;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 
 public class login extends AppCompatActivity {
     ImageView return_main1;
-
+  Button go_login_to_register;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         return_main1= findViewById(R.id.image_back);
+        go_login_to_register=findViewById(R.id.butt_login);
     }
 
     public void return_main(View view) {
         onBackPressed();
 
+    }
+
+    public void go_to_home(View view) {
+        Intent go_home = new Intent(this,view_speed_bump.class);
+        startActivity(go_home);
     }
 }
--- app/src/main/java/com/example/taraffac/register.java
+++ app/src/main/java/com/example/taraffac/register.java
@@ -2,21 +2,30 @@ package com.example.taraffac;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 
 public class register extends AppCompatActivity {
     ImageView return_main2;
+    Button go_register_to_home1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
         return_main2= findViewById(R.id.image_back2);
+        go_register_to_home1=findViewById(R.id.butt_register);
     }
 
     public void return_main2(View view) {
         onBackPressed();
+    }
+
+    public void go_to_home1(View view) {
+        Intent go_home1 = new Intent(this,view_speed_bump.class);
+        startActivity(go_home1);
     }
 }
