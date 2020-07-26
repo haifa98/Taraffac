6c9582dac1fbdd2ee2df36ff7b872e18c6358c3c
Amjad1418 <amjad.nasser.al@gmail.com>
2020-07-26 08:25:06 +0300

    all class

---
+++ app/src/main/java/com/example/taraffac/view_spped_bump_msg.java
@@ -0,0 +1,14 @@
+package com.example.taraffac;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class view_spped_bump_msg extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_spped_bump_msg);
+    }
+}
