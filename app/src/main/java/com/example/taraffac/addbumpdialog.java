c7e65e620bce606d3ceae0a15f7a6e1fa3ab9706
haifa <haifa_alkhudair@hotmail.com>
2020-09-26 14:47:59 +0300

    .

--- app/src/main/java/com/example/taraffac/addbumpdialog.java
+++ app/src/main/java/com/example/taraffac/addbumpdialog.java
@@ -12,6 +12,8 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatDialogFragment;
 
+
+
 public class addbumpdialog extends AppCompatDialogFragment {
     private RadioGroup type;
     private RadioGroup size;
