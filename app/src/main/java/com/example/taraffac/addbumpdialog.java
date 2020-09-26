4b9b90befa5a304cbe99bfd78d6f5906fa66e0b3
haifa <haifa_alkhudair@hotmail.com>
2020-09-26 14:41:40 +0300

    .

---
+++ app/src/main/java/com/example/taraffac/addbumpdialog.java
@@ -0,0 +1,44 @@
+package com.example.taraffac;
+
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.RadioGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatDialogFragment;
+
+public class addbumpdialog extends AppCompatDialogFragment {
+    private RadioGroup type;
+    private RadioGroup size;
+    //type = (RadioGroup)findViewById(R.id.type);
+    @NonNull
+    @Override
+    public Dialog onCreateDialog(@Nullable Bundle savedInstanceState) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        LayoutInflater inflater = getActivity().getLayoutInflater();
+        View view = inflater.inflate(R.layout.addbump, null);
+        builder.setView(view)
+                .setTitle("Add Speed bump")
+                .setNegativeButton("cancel", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+
+                    }
+                })
+                .setPositiveButton("Save", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+
+                    }
+                });
+                type =  view.findViewById(R.id.type);
+                size =  view.findViewById(R.id.size);
+
+return builder.create();
+    }
+}
