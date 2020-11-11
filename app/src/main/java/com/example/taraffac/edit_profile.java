7af1c43e9b4e1a4bd81bf59dc1bd4d833366d376
Amjad1418 <amjad.nasser.al@gmail.com>
2020-11-11 22:55:28 +0300

    // edit Profile1

--- app/src/main/java/com/example/taraffac/edit_profile.java
+++ app/src/main/java/com/example/taraffac/edit_profile.java
@@ -114,6 +114,7 @@ public class edit_profile extends AppCompatActivity {
                        edited.put("email",ProfileEditEmail.getText().toString());
                         edited.put("Name",ProfileEditFullName.getText().toString());
                         ////
+                        ////
 
                         docRef.update(edited).addOnSuccessListener(new OnSuccessListener<Void>() {
                             @Override
